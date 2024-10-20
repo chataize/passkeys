b39588e66c538603af151194672468a748491815
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-20 23:32:47 +0200

    Implement createPasskey and getPasskey Javascript metods

--- src/wwwroot/passkeys.js
+++ src/wwwroot/passkeys.js
@@ -1,7 +1,45 @@
-export async function createPasskey() {
+export async function createPasskey(domain, appName, userId, userName, userDisplayName, challenge) {
+    const encoder = new TextEncoder();
+    const publicKey = {
+        challenge: challenge,
+        rp: {
+            name: appName,
+            id: domain
+        },
+        user: {
+            id: encoder.encode(userId),
+            name: userName,
+            displayName: userDisplayName
+        },
+        pubKeyCredParams: [
+            { type: "public-key", alg: -7 }
+        ],
+    };
 
+    const credential = await navigator.credentials.create({ publicKey });
+    const credentialId = btoa(String.fromCharCode(...new Uint8Array(credential.rawId)));
+    const attestationBase64 = btoa(String.fromCharCode(...new Uint8Array(credential.response.attestationObject)));
+    const clientDataJSON = btoa(String.fromCharCode(...new Uint8Array(credential.response.clientDataJSON)));
+
+    return {
+        credentialId: credentialId,
+        attestation: attestationBase64,
+        clientDataJSON: clientDataJSON
+    };
 }
 
-export async function getPasskey() {
+export async function getPasskey(domain, challenge) {
+    const publicKey = { challenge: challenge, rpId: domain };
+    const credential = await navigator.credentials.get({ publicKey });
+    const credentialId = btoa(String.fromCharCode(...new Uint8Array(credential.rawId)));
+    const authenticatorData = btoa(String.fromCharCode(...new Uint8Array(credential.response.authenticatorData)));
+    const clientDataJSON = btoa(String.fromCharCode(...new Uint8Array(credential.response.clientDataJSON)));
+    const signature = btoa(String.fromCharCode(...new Uint8Array(credential.response.signature)));
 
+    return {
+        credentialId: credentialId,
+        authenticatorData: authenticatorData,
+        clientDataJSON: clientDataJSON,
+        signature: signature
+    };
 }
