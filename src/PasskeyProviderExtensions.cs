de39075d3b2a40dc0577dcfc55653cce94bb3291
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-20 23:33:21 +0200

    Make configure parameter mandatory

--- src/PasskeyProviderExtensions.cs
+++ src/PasskeyProviderExtensions.cs
@@ -4,13 +4,9 @@ namespace ChatAIze.Passkeys;
 
 public static class PasskeyProviderExtensions
 {
-    public static IServiceCollection AddPasskeyProvider(this IServiceCollection services, Action<PasskeyOptions>? configure = null)
+    public static IServiceCollection AddPasskeyProvider(this IServiceCollection services, Action<PasskeyOptions> configure)
     {
-        if (configure is not null)
-        {
-            services.Configure(configure);
-        }
-
+        services.Configure(configure);
         return services.AddScoped<PasskeyProvider>();
     }
 }
