c6529b55b6f89aabea271fc1cf4956b4e7e9168d
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-21 09:16:27 +0200

    Make several Passkey record properties internal

--- src/Passkey.cs
+++ src/Passkey.cs
@@ -4,17 +4,17 @@ public sealed record Passkey
 {
     public required byte[]? CredentialId { get; set; }
 
-    public byte[]? Challenge { get; set; }
-
-    public byte[]? AuthenticatorData { get; set; }
-
-    public byte[]? ClientDataJson { get; set; }
-
-    public byte[]? Signature { get; set; }
-
     public byte[]? PublicKey { get; set; }
 
     public string? CredentialIdBase64 => CredentialId is not null ? Convert.ToBase64String(CredentialId) : null;
 
     public string? PublicKeyBase64 => PublicKey is not null ? Convert.ToBase64String(PublicKey) : null;
+
+    public byte[]? Challenge { get; set; }
+
+    internal byte[]? AuthenticatorData { get; set; }
+
+    internal byte[]? ClientDataJson { get; set; }
+
+    internal byte[]? Signature { get; set; }
 }
