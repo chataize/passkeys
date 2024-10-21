bb22e60e8a94a960981bf312e0a868c2c5a2c82b
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-21 09:17:24 +0200

    Make passkey record immutable

--- src/Passkey.cs
+++ src/Passkey.cs
@@ -2,19 +2,19 @@ namespace ChatAIze.Passkeys;
 
 public sealed record Passkey
 {
-    public required byte[]? CredentialId { get; set; }
+    public required byte[]? CredentialId { get; init; }
 
-    public byte[]? PublicKey { get; set; }
+    public byte[]? PublicKey { get; init; }
 
     public string? CredentialIdBase64 => CredentialId is not null ? Convert.ToBase64String(CredentialId) : null;
 
     public string? PublicKeyBase64 => PublicKey is not null ? Convert.ToBase64String(PublicKey) : null;
 
-    public byte[]? Challenge { get; set; }
+    public byte[]? Challenge { get; init; }
 
-    internal byte[]? AuthenticatorData { get; set; }
+    internal byte[]? AuthenticatorData { get; init; }
 
-    internal byte[]? ClientDataJson { get; set; }
+    internal byte[]? ClientDataJson { get; init; }
 
-    internal byte[]? Signature { get; set; }
+    internal byte[]? Signature { get; init; }
 }
